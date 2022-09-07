30189ac24189064082c7537a40138565441fc8db
Alina Saralidze <alina.saralina@gmail.com>
2022-09-07 16:27:33 +0300

    done_very_bad

--- file.php
+++ file.php
@@ -1,17 +1,29 @@
 <?php
-echo "<link rel='stylesheet' href='style.css'>";
 $a = 0;
-$b = 1;
-$rezult1 = $a < $b;
-$rezult2 = $a > $b;
-$rezult3 = $a != $b;
-$rezult4 = $a || $b;
-$result5 = $a && $b;
-$result6 = $a xor $b;
-
-echo "</head>
-   <body>
-    <table>
+$b = 0;
+$rez1 = !($a > $b);
+$rez2 = $a || $b;
+$rez3 = $a && $b;
+$rez4 = $a xor $b;
+$c = 0;
+$d = 1;
+$rez5 = !($c > $d);
+$rez6 = $c || $d;
+$rez7 = $c && $d;
+$rez8 = $c xor $d;
+$e = 1;
+$f = 0;
+$rez9 = !($e > $f);
+$rez10 = $e || $f;
+$rez11 = $e && $f;
+$rez12 = $e xor $f;
+$g = 1;
+$h = 1;
+$rez9 = !($g > $h);
+$rez10 = $g || $h;
+$rez11 = $g && $h;
+$rez12 = $g xor $h;
+echo "<table>
         <thead>
             <tr>
             <th><strong>A</strong></th>
@@ -24,38 +36,40 @@ echo "</head>
         </thead>
         <tbody>
             <tr>
-            <td>".$rezult1."</td>
-            <td>".$rezult2."</td>
-            <td>".$rezult3."</td>
-            <td>".$rezult4."</td>
-            <td>".$result5."</td>
-            <td>".$result6."</td>
+            <td>0</td>
+            <td>0</td>
+            <td>$rez1</td>
+            <td>$rez2</td>
+            <td>$rez3</td>
+            <td>$rez4</td>
             </tr>
             <tr>
             <td>0</td>
             <td>1</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>$rez5</td>
+            <td>$rez6</td>
+            <td>$rez7</td>
+            <td>$rez8</td>
             </tr>
             <tr>
             <td>1</td>
             <td>0</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>$rez9</td>
+            <td>$rez10</td>
+            <td>$rez11</td>
+            <td>$rez12</td>
             </tr>
             <tr>
             <td>1</td>
             <td>1</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>$rez9</td>
+            <td>$rez10</td>
+            <td>$rez11</td>
+            <td>$rez12</td>
             </tr>
             </tbody>
-    </table>
-   </body>";
+    </table>";
+?>
+<?php
+echo "<link rel = 'stylesheet' href = 'style.css'>";
 ?>
