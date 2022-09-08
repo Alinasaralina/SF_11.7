7c129fb24f4cde57612b7495ab645df855499656
Alina Saralidze <alina.saralina@gmail.com>
2022-09-08 09:31:28 +0300

    add_task_2

---
+++ task_2.php
@@ -0,0 +1,207 @@
+<?php
+echo 'Гибкое сравнение с помощью ==';
+$x= true;
+$y= false;
+$result = ($x==$y);
+?>;
+<?php
+echo '<table style="table-layout: auto; width: 900px; margin: auto;">
+<thead>
+<tr><th width="10px"></th><th width="10px">true</th><th width="10px">false</th><th width="10px">1</th><th width="10px">0</th><th width="10px">-1</th><th width="10px">"1"</th><th width="10px">null</th><th width="10px">"php"</th></tr>
+</thead>
+<tbody>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>true</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>false</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>1</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>0</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>-1</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>"1"</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>null</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="font-size: 16px; white-space: nowrap; background-color: #f5f5f5;"><strong>"php"</strong></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+</tr>
+</tbody>
+</table>';
+?>
+<?php
+echo "<link rel = 'stylesheet' href = 'task_2.css'>";
+?>
+<?php
+echo 'Жесткое сравнение  с помощью ===';
+echo '<table style="table-layout: auto; width: 900px; margin: auto;">
+<thead>
+<tr><th width="10px"></th><th width="10px">true</th><th width="10px">false</th><th width="10px">1</th><th width="10px">0</th><th width="10px">-1</th><th width="10px">"1"</th><th width="10px">null</th><th width="10px">"php"</th></tr>
+</thead>
+<tbody>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>true</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>false</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>1</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>0</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>-1</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>"1"</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="background-color: #f5f5f5;"><strong>null</strong></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td style="font-size: 16px; white-space: nowrap; background-color: #f5f5f5;"><strong>"php"</strong></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+<td style="font-size: 16px; white-space: nowrap;"></td>
+</tr>
+</tbody>
+</table>';
+?>
+<?php
+echo "<link rel = 'stylesheet' href = 'task_2.css'>";
+?>
